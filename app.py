9f42e7ea37d38e43542dde903c64436c2fbc87d2
Grant Hohol <ghohol@wisc.edu>
2024-07-28 15:26:34 -0500

    Adding genres excludes

--- app.py
+++ app.py
@@ -152,6 +152,20 @@ def print_recs(movies_no_user: pd.DataFrame):
     )
     movies_no_user = movies_no_user[movies_no_user[genre_filter].all(axis=1)]
 
+    genre_none_filter = st.sidebar.multiselect(
+        "Must have genre(s)",
+        ['Action', 'Adventure', 'Animation', 'Comedy', 'Crime', 'Documentary', 'Drama', 'Family', 'Fantasy', 'History', 'Horror', 
+         'Music', 'Mystery', 'Romance', 'Science Fiction', 'Thriller', 'War', 'Western']
+    )
+    
+    # Function to check if a movie has any of the excluded genres
+    def has_excluded_genre(genres, excluded_genres):
+        return any(genre in genres for genre in excluded_genres)
+
+# Filter the DataFrame to exclude movies with the selected genres
+    if genre_none_filter:
+        movies_no_user = movies_no_user[~movies_no_user['genres'].apply(has_excluded_genre, excluded_genres=genre_none_filter)]
+
     # Text input for minimum release year
     year_min = st.sidebar.text_input("Minimum Release Year", "1900")
     movies_no_user = movies_no_user[movies_no_user['date'] >= year_min]
